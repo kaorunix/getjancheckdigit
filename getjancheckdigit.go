ab8192daa9706efe1843bda415699897ce4aeb55
Kaoru Sato <k.sato@creist.co.jp>
2019-10-21 09:26:03 +0900

    code sample

---
+++ getjancheckdigit.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(jan string) {
+     int count := len(jan)
+     switch count {
+     	    case 8:
+	    case 13:
+	    default:
+	fmt.Println("Length error ")
+}
+
+
+func 
